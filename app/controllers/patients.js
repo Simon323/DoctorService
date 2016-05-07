8e0dec9bbf6e1eadd40a99e184501f50246117a8
Piotr Krusicki <piotr.krusicki@gmail.com>
2016-05-07 12:45:52 +0200

    doctors-patients relation

--- app/controllers/patients.js
+++ app/controllers/patients.js
@@ -1,42 +1,109 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  User = require('../models/user')
+  User = require('../models/user');
 
 module.exports = function (app) {
-  app.use('/patients', router);
+  app.use('/patient', router);
 };
 /*
-* GET all patients
-*/
+ * GET all patients
+ */
 router.get('/', function (req, res, next) {
   //returns all the patients
-  User.find({'role':'patient'},function(err,users){
+  User.find({'role': 'patient'}, function (err, users) {
     res.send(users);
   })
 });
 
 /*
-* GET doctors of patient with id..
-*/
-router.get('/:patient_id/doctors', function (req, res, next) {
-  //todo pobieranie doctors danego pacjenta -get
+ * GET doctors of patient with id..
+ */
+router.get('/doctors', function (req, res, next) {
+  // var patient_id = mongoose.Types.ObjectId(req.params["patient_id"]);
+  var patient_id = mongoose.Types.ObjectId(req.user.id);
+
+  var patientDoctors = [];
+  var restDoctors = [];
+
+  User.findOne({'_id': patient_id})
+    .populate('doctors')
+    .exec(function (err, patient) {
+      if (err) {res.sendStatus(400)}
+      else {
+        patientDoctors = patient.doctors;
+        User.find({'role': 'doctor'})
+          // .populate('patients')
+          .exec(function (err, doctors) {
+            if (err) {res.sendStatus(400)}
+            else {
+              doctors.forEach(function (d) {
+                if (d.patients.indexOf(patient_id) == -1) {
+                  restDoctors.push(d)
+                }})
+            }
+            res.send(200, {"patientDoctors" : patientDoctors , "restDoctors" : restDoctors})
+          })}
+    })
 });
 
 /*
  * ADD a new doctor do patient with id ..
  */
-router.post('/:patient_id/doctors', function (req, res, next) {
-  //todo dodawanie doctors danego pacjenta - post
+router.post('/doctors', function (req, res, next) {
+  // var patient_id = mongoose.Types.ObjectId(req.params["patient_id"]);
+  var patient_id = mongoose.Types.ObjectId(req.user.id);
+  var doctor_id  = mongoose.Types.ObjectId(req.body.doctor_id);
+
+  User.findOne({'_id': patient_id}, function (err, patient) {
+    if (err) {
+      res.sendStatus(200)
+    }
+    else {
+      User.findOne({'_id': doctor_id}, function (err, doctor) {
+        if (err) {
+          res.senStatus(400)
+        }
+        else {
+          patient.doctors.push(doctor);
+          doctor.patients.push(patient);
+          patient.save(function (err, p) {
+          });
+          doctor.save(function (err, d) {
+          });
+          res.sendStatus(200);
+        }
+      })
+    }
+  });
 });
 
 /*
  * DELETE a doctor from array that belongs to patient with id..
  */
-router.delete('/:patient_id/doctors', function (req, res, next) {
-  //todo usuwanie docotrs danego pacjenta -delete
-});
+router.delete('/doctors', function (req, res, next) {
+  var patient_id = mongoose.Types.ObjectId(req.user.id);
+  var doctor_id = mongoose.Types.ObjectId(req.body.doctor_id);
 
+  User.update(
+    {'_id' : patient_id},
+    {$pull : {doctors : doctor_id}},
+    {safe : true},
+    function(err, patient){
+      if(err){throw err}
+      else {
+        User.update(
+          {'_id' : doctor_id},
+          {$pull : {patients : patient_id}},
+          {safe : true},
+          function(err,doctor){
+            if(err){throw err}
+            else {res.sendStatus(200)}
+          })
+      }
+    }
+  )
+  });
 
 
 
--- public/js/angular/patient_panel.js
+++ public/js/angular/patient_panel.js
@@ -1,43 +1,69 @@
 var patient_panel = angular.module('patient_panel',[]);
 
-patient_panel.controller('patientPanelCtrl',['$scope',function($scope){
-  
+patient_panel.controller('patientPanelCtrl',['$scope','$http',function($scope,$http){
+
   /*
-  * Doctors access controll 
+  * Doctors access controll
   */
-  
-  $scope.allDoctors  = getAllDoctors();
-  $scope.userDoctors = getUserDoctors();
-  $scope.restDoctors = getRestDoctors($scope.allDoctors, $scope.userDoctors);
 
-  $scope.addDoctors = function(doctors){
-    //todo request
-    //todo dodawanie do list userDoctors i restDoctors
+  $scope.patientDoctors = [];
+  $scope.restDoctors = [];
+
+  getDoctors();
+
+  $scope.addDoctors = function(fromRestDoctors){
+    fromRestDoctors.forEach(function(doctor){
+      $http({method: 'POST', url: 'http://localhost:3000/patient/doctors',data: {'doctor_id' : doctor._id}})
+        .then(
+          function successCallback(response) {
+            //przekazywany jest jedynie id, a chcemy dodac caly obiekt do listy patientDoctors
+            $scope.patientDoctors.push(doctor);
+            $scope.restDoctors.splice($scope.restDoctors.indexOf(doctor),1)
+            console.log("add doctors ok");
+          },
+          function errorCallback(response) {
+            console.log('addDoctors fail');
+          });
+    })
   };
-  
-  $scope.removeDoctors = function(doctors){
-    //todo request
-    //todo usuwanie z list userDoctors i restDoctors
+
+  $scope.removeDoctors = function(fromPatientDoctors){
+    fromPatientDoctors.forEach(function(doctor){
+      $http({method: 'DELETE', url: 'http://localhost:3000/patient/doctors',headers: {'Content-Type': 'application/json'},data: {'doctor_id' : doctor._id}})
+        .then(
+          function successCallback(response) {
+            //todo usunac z listy patientDoctors dodac do restDoctors
+            $scope.restDoctors.push(doctor);
+            $scope.patientDoctors.splice($scope.patientDoctors.indexOf(doctor),1);
+            console.log("removeDoctors ok");
+          },
+          function errorCallback(response) {
+            console.log('addDoctors fail');
+          });
+    })
   };
-  function getAllDoctors() {
-    //todo http get /doctors
+
+  function getDoctors() {
+    $http({method: 'GET', url: 'http://localhost:3000/patient/doctors'})
+      .then(
+        function successCallback(response) {
+          $scope.patientDoctors = response.data.patientDoctors;
+          $scope.restDoctors = response.data.restDoctors;
+        },
+
+        function errorCallback(response) {
+          console.log('getDoctors fail');
+      });
+
   }
 
-  function getUserDoctors() {
-    //todo http get /patient/:id/doctors
-  }
-
-  function getRestDoctors(allDoctors,userDoctors) {
-    //todo roznica
-  }
-  
   /*
   *  Health story
   */
-  
+
   $scope.healthStory = getUserHealthStory();
-  
+
   function getUserHealthStory(){
-    //todo pobieranie historii choroby  
+    //todo pobieranie historii choroby
   }
 }]);
